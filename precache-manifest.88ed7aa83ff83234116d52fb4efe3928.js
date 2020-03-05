3177b49cd98b4b97a36f11a645568de2fbb7f82c
bishoy adly <tobbeywilliam@gmail.com>
2020-03-05 23:08:31 +0200

    Updates

---
+++ precache-manifest.88ed7aa83ff83234116d52fb4efe3928.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f87ee1c11638a4de54429536c66fa00",
+    "url": "/email-client-frontend/index.html"
+  },
+  {
+    "revision": "3b0f20820bc3bed10f2c",
+    "url": "/email-client-frontend/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "d7672be0340014f798ac",
+    "url": "/email-client-frontend/static/css/main.bae25071.chunk.css"
+  },
+  {
+    "revision": "3b0f20820bc3bed10f2c",
+    "url": "/email-client-frontend/static/js/2.0fd2afd3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/email-client-frontend/static/js/2.0fd2afd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7672be0340014f798ac",
+    "url": "/email-client-frontend/static/js/main.d2a9ca3a.chunk.js"
+  },
+  {
+    "revision": "c6463ee79e5e82772f72",
+    "url": "/email-client-frontend/static/js/runtime-main.6d47e059.js"
+  },
+  {
+    "revision": "0239bd87b0e44bbed4f41976c5f21a7d",
+    "url": "/email-client-frontend/static/media/email-image.0239bd87.png"
+  }
+]);
